e675f2aa4b87d8e1bb04e6704c0ed39e83c157d8
Smriti Garg <smriti.garg@meredith.com>
2022-05-05 18:30:27 +0530

    Basic Setup and cleanup

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { PostCard, PostWidget, Categories } from '../components'
+const posts =[
+  {title: "GraphQL", excerpt: "Learn GraphQL"},
+  {title: "GraphQL with NextJS", excerpt: "Learn GraphQL with NextJS"}
+
+]
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>CMS BLOG</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <div className='grid grid-cols-1 lg:grid-cols-12 gap-12' >
+      <div className='lg:col-span-8 col-span-1'>
+      {posts.map((post)=>(
+        <PostCard key={post.title} post={post}/>
+      ))}
+      </div>
+      <div className='lg:col-span-4 col-span-1' >
+        <div className='lg:sticky relative top-8' >
+          <PostWidget/>
+          <Categories/>
+        </div>
+      </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Home
