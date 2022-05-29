e0dbc68864b0dde250f841f90269a9dccd96da99
Smriti Garg <smriti.garg@meredith.com>
2022-05-29 19:14:53 +0530

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components';
 import {getPosts} from '../services';
 import FeaturedPost from '../sections/FeaturedPost'
+import { Key } from 'react';
 
-export default function Home ({posts}){
+export default function Home ({posts}:any){
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -13,7 +14,7 @@ export default function Home ({posts}){
       <FeaturedPost/>
     <div className='grid grid-cols-1 lg:grid-cols-12 gap-12' >
       <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post)=>(
+        {posts.map((post: { node: { id: Key | null | undefined; }; })=>(
           <div key={post.node.id} >
             <PostCard post={post.node} />
           </div>
@@ -21,7 +22,7 @@ export default function Home ({posts}){
       </div>
       <div className='lg:col-span-4 col-span-1' >
         <div className='lg:sticky relative top-8' >
-          <PostWidget/>
+          <PostWidget categories={null} slug={null}/>
           <Categories/>
         </div>
       </div>
